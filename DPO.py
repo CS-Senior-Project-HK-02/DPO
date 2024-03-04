992f6ed3a9f55dbe074d65752c60d1f2075fb9f3
hnoelw <102485249+hnoelw@users.noreply.github.com>
2024-03-04 01:56:19 -0500

    Update DPO.py to use unsloth

--- DPO.py
+++ DPO.py
@@ -1,41 +1,43 @@
-from datasets import load_dataset
-from collections import defaultdict 
-class DataInstruct:
-    name = r"/home/hwalke37/dataset/data_instruct.json"
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "4" 
+os.environ["WORLD_SIZE"] = "1"
+import torch
+from transformers import TrainingArguments
+from trl import DPOTrainer
+from unsloth import FastLanguageModel
 
-    def __init__(self, split: str = "train"):
-        super().__init__()
-        self.split = split
-        dataset = load_dataset(self.name, split=self.split)
-        self.dataset_dict = defaultdict(dict)
-        for item in dataset:
-            post_id = item["question"]["id"]
-            if post_id not in self.dataset_dict.keys():
-                self.dataset_dict[post_id] = {
-                    "full_text": item["question"]["full_text"],
-                    "answers": [],
-                }
-                if item["score_0"] > 0:
-                    answers = [item["answer_0"], item["answer_1"]]
-                elif item["score_0"] < 0:
-                    answers = [item["answer_1"], item["answer_0"]]
-                else:
-                    answers = []
-                answers = [re.sub(r"\[\d+\]", "", answer) for answer in answers]
-                answers = [
-                    ".".join([sent.strip() for sent in answer.split(".")])
-                    for answer in answers
-                ]
-                if answers:
-                    self.dataset_dict[post_id]["answers"].extend(answers)
-                else:
-                    _ = self.dataset_dict.pop(post_id)
+max_seq_length = 2048 # Supports automatic RoPE Scaling, so choose any number.
 
-        self.post_ids = list(self.dataset_dict.keys())
+# Load model
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "tiny-llama-pn-annotate-hkllm-ksu-internal",
+    max_seq_length = max_seq_length,
+    dtype = None, # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit = True, # Use 4bit quantization to reduce memory usage. Can be False.
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
 
-    def __len__(self):
-        return len(self.post_ids)
+# Do model patching and add fast LoRA weights
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16,
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Dropout = 0 is currently optimized
+    bias = "none",    # Bias = "none" is currently optimized
+    use_gradient_checkpointing = True,
+    random_state = 3407,
+)
 
-    def __getitem__(self, idx):
-        question, answers = self.dataset_dict[self.post_ids[idx]].values()
-        return question, answers
+training_args = TrainingArguments(output_dir="./DPOoutput")
+
+dpo_trainer = DPOTrainer(
+    model,
+    model_ref=None,
+    args=training_args,
+    beta=0.1,
+    train_dataset=train_dataset,
+    tokenizer=tokenizer,
+)
+dpo_trainer.train()
